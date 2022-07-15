73ff913e40d12a2e5d5b3c567c07940283acd297
Dmitrii Boikov <boikovdv@gmail.com>
2022-07-15 15:25:31 +0300

    refactor checkbox component

--- pl-checkbox.js
+++ pl-checkbox.js
@@ -3,8 +3,7 @@ import { PlElement, html, css } from "polylib";
 import "@plcmp/pl-labeled-container";
 
 class PlCheckbox extends PlElement {
-    static get properties() {
-        return {
+    static properties = {
             label: { type: String },
             variant: { type: String },
             orientation: { type: String },
@@ -13,11 +12,10 @@ class PlCheckbox extends PlElement {
             disabled: { type: Boolean, reflectToAttribute: true },
             hidden: { type: Boolean, reflectToAttribute: true },
             checked: { type: Boolean, observer: '_checkedObserver' }
-        }
-    }
+    };
 
-    static get css() {
-        return css`
+
+    static css = css`
             :host {
                 display: inline-block;
                 outline: none;
@@ -93,7 +91,7 @@ class PlCheckbox extends PlElement {
                 border: 1px solid var(--primary-base);
             }
 
-            .checkbox.checked {
+            .checkbox[state=checked] {
                 background: var(--primary-base);
                 border: none;
                 background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none"%3E%3Cpath d="M1 4.5L3 7L7.5 1" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
@@ -101,7 +99,7 @@ class PlCheckbox extends PlElement {
                 background-position: center;
             }
 
-            .checkbox.checked:hover {
+            .checkbox[state=checked]:hover {
                 background: var(--primary-dark);
                 border: none;
                 background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="9" height="8" viewBox="0 0 9 8" fill="none"%3E%3Cpath d="M1 4.5L3 7L7.5 1" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
@@ -109,12 +107,12 @@ class PlCheckbox extends PlElement {
                 background-position: center;
             }
 
-            .checkbox.indeterminate {
+            .checkbox[state=indeterminate] {
                 background: var(--white);
                 border: 1px solid var(--grey-base);
             }
 
-            .checkbox.indeterminate:after {
+            .checkbox[state=indeterminate]:after {
                 display: block;
                 content: '';
                 background: var(--primary-base);
@@ -125,17 +123,14 @@ class PlCheckbox extends PlElement {
                 left: 3px;
             }
 
-            .checkbox.indeterminate:hover {
+            .checkbox[state=indeterminate]:hover {
                 border: 1px solid var(--grey-dark);
             }
-		`;
-    }
+    `;
 
 
     connectedCallback() {
         super.connectedCallback();
-        this._checkboxContainer = this.root.querySelector('.checkbox');
-        this._checkedObserver();
 
         if (this.variant) {
             console.log('Variant is deprecated, use orientation instead');
@@ -143,31 +138,24 @@ class PlCheckbox extends PlElement {
         }
     }
 
-    static get template() {
-        return html`
+    static template = html`
             <pl-labeled-container label="[[label]]" orientation="[[orientation]]">
                 <slot name="label-prefix" slot="label-prefix"></slot>
                 <div class="checkbox-container" on-click="[[_onClick]]">
-                    <div class="checkbox"></div>
+                    <div id="checkbox" class="checkbox"></div>
                     <div class="caption">[[caption]]</div>
                 </div>
                 <slot name="label-suffix" slot="label-suffix"></slot>
             </pl-labeled-container>
-		`;
-    }
+    `;
 
-    _checkedObserver() {
-        if (this.checked === undefined) {
-            this._checkboxContainer.classList.add('indeterminate');
-        } else {
-            this._checkboxContainer.classList.remove('indeterminate');
-        }
-
-        if (this.checked) {
-            this._checkboxContainer.classList.add('checked');
-        } else {
-            this._checkboxContainer.classList.remove('checked');
-        }
+    _checkedObserver(v) {
+        if (v === undefined || v === null)
+            this.$.checkbox.setAttribute('state', 'indeterminate');
+        else if (v)
+            this.$.checkbox.setAttribute('state', 'checked');
+        else
+            this.$.checkbox.setAttribute('state', 'unchecked');
     }
 
     _onClick() {
